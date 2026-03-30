afc84d05f3ada419c023492cdd7be78e6575f6a2
abdauahmohamed1-wq <abdauah.mohamed1@gmail.com>
2026-03-30 15:45:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,12 +9,29 @@ self.addEventListener('install', (e) => {
       ]);
     })
   );
+  // إضافة: تجعل النسخة الجديدة من التطبيق تحل محل القديمة فوراً
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
       return response || fetch(e.request);
+    })
+  );
+});
+
+// إضافة: تنظيف الـ Cache القديم عند تحديث الإصدار (عشان التعديلات تظهر)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== 'abu-karma-v1') {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
